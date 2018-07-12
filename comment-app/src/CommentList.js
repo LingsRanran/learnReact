20d25ef748b17d22405a32d28197d6efba08c4b2
LingsRanran <1078421081@qq.com>
2018-07-12 18:57:19 +0800

    4th commit

--- comment-app/src/CommentList.js
+++ comment-app/src/CommentList.js
@@ -2,16 +2,17 @@ import React,{Component} from 'react';
 import Comment from './Comment';
 
 class CommentList extends Component{
+    static defaultProps = {
+        comments : []
+    }
+
     render(){
-        const comments = [
-            {userName: 'Jerry', content: 'Hello'},
-            {userName: 'Tomy', content: 'World'},
-            {userName: 'Lucy', content: 'Good'}
-        ];
 
         return (
             <div>
-                {comments.map((comment, i) => <Comment comment={comment} key={i} />)}
+                {
+                    this.props.comments.map((comment,i) => <Comment comment={comment} key={i} />)    
+                }
             </div>
         )
     }
--- comment-app/src/index.js
+++ comment-app/src/index.js
@@ -1,11 +1,87 @@
-import React from 'react';
+import React  from 'react';
 import ReactDOM from 'react-dom';
 import CommentApp from './CommentApp'
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+/* class Input extends Component{
+    constructor(){
+        super();
+        this.state = {
+            userInput : 0
+        }
+    }
+
+    handleInputChange(e){
+        if(!e.target.value){
+            e.target.value = 0;
+        }
+
+        console.log(e.target.value)
+
+        if(this.props.inputChange){
+            this.props.inputChange(e.target.value);
+        }
+
+        this.setState({
+            userInput : e.target.value
+        })
+    }
+
+    render(){
+        return (
+            <input type="number" value={this.state.userInput} onChange={this.handleInputChange.bind(this)} />
+        )
+    }
+}
+
+class PercentageShower extends Component{
+    turnNum(num){
+        num = parseFloat(num);
+
+        num *= 100;
+
+        return num.toFixed(2);
+    }
+
+    render(){
+        return (
+            <div>{this.turnNum(this.props.userInput)}%</div>
+        )
+    }
+}
+
+class PercentageApp extends Component{
+    constructor(){
+        super();
+        this.state = {
+            userInput : 0
+        }
+    }
+
+    handleInputChangeOnApp(val){
+        this.setState({
+            userInput : val
+        })
+    }
+
+    render(){
+        return (
+            <div>
+                <Input inputChange={this.handleInputChangeOnApp.bind(this)} />
+                <PercentageShower userInput={this.state.userInput} />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <PercentageApp />,
+    document.getElementById('root')
+) */
 ReactDOM.render(
     <CommentApp />,
     document.getElementById('root')
 )
+
 registerServiceWorker();
